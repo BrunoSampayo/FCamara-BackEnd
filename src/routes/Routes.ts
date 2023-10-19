9f52e9822984e7e715347411aa8dceef43af7504
Bruno Sampayo <brunosampayo1040@gmail.com>
2023-10-19 20:15:58 -0300

    feat: add get Users Router

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -2,16 +2,23 @@ import { Router } from "express";
 import { CreateUserController } from "../useCases/user/createUser/CreateUserController";
 import { AuthenticateUserController } from "../useCases/user/authenticateUser/AuthenticateUserController";
 import { DeleteUserController } from "../useCases/user/deleteUser/DeleteUserController";
+import { GetUserController } from "../useCases/user/getUser/GetUserController";
+
 import { AuthHandler } from "../middlewares/EnsureAuth";
 import { EnsureAsAdmin } from "../middlewares/EnsureAsAdminstrator";
+
 export const router = Router();
 
 const createUserControler = new CreateUserController;
 const authenticateUserController = new AuthenticateUserController;
 const deleteUserController = new DeleteUserController;
+const getUserController = new GetUserController;
 
 
 router.get('/ping', (req, res) => { res.json({ pong: true }) });
-router.post('/user',AuthHandler,EnsureAsAdmin, createUserControler.handle);
+
+router.post('/user', AuthHandler, EnsureAsAdmin, createUserControler.handle);
+router.delete('/user', AuthHandler, EnsureAsAdmin, deleteUserController.handle);
+router.get('/user', getUserController.handle);
 router.post('/login', authenticateUserController.handle);
-router.delete('/user', AuthHandler,EnsureAsAdmin,deleteUserController.handle);
+
