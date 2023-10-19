801ea7a0de7396f51b30dfc2296e99265cb0407e
Bruno Sampayo <brunosampayo1040@gmail.com>
2023-10-19 19:07:56 -0300

    feat: add routes deleteUser, and middleware usage

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -1,12 +1,18 @@
 import { Router } from "express";
 import { CreateUserController } from "../useCases/user/createUser/CreateUserController";
 import { AuthenticateUserController } from "../useCases/user/authenticateUser/AuthenticateUserController";
+import { DeleteUserController } from "../useCases/user/deleteUser/DeleteUserController";
+import { AuthHandler } from "../middlewares/EnsureAuth";
+import { EnsureAsAdmin } from "../middlewares/EnsureAsAdminstrator";
+export const router = Router();
 
-export const router= Router();
+const createUserControler = new CreateUserController;
+const authenticateUserController = new AuthenticateUserController;
+const deleteUserController = new DeleteUserController;
 
-const createUserControler= new CreateUserController;
-const authenticateUserController= new AuthenticateUserController;
-router.get('/ping', (req, res) => {res.json({pong:true})})
-router.post('/user',createUserControler.handle);
-router.post('/login', authenticateUserController.handle )
-router.get("/teste", (req, res) => {throw new Error("Teste")})
+
+router.get('/ping', (req, res) => { res.json({ pong: true }) });
+router.post('/user', createUserControler.handle);
+router.post('/login', authenticateUserController.handle);
+router.delete('/user', AuthHandler,EnsureAsAdmin,deleteUserController.handle);
+router.get("/teste", (req, res) => { throw new Error("Teste") });
