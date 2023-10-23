66e3e80dcd99ae0fbd544d3a826ed8111fb035f6
Bruno Sampayo <brunosampayo1040@gmail.com>
2023-10-23 15:19:06 -0300

    feat: Router from edit user

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+
 import { CreateUserController } from "../useCases/user/createUser/CreateUserController";
 import { AuthenticateUserController } from "../useCases/user/authenticateUser/AuthenticateUserController";
 import { DeleteUserController } from "../useCases/user/deleteUser/DeleteUserController";
 import { GetUserController } from "../useCases/user/getUser/GetUserController";
+import { EditUserController } from "../useCases/user/editUser/EditUserController";
 
 import { AuthHandler } from "../middlewares/EnsureAuth";
 import { EnsureAsAdmin } from "../middlewares/EnsureAsAdminstrator";
@@ -13,12 +15,14 @@ const createUserControler = new CreateUserController;
 const authenticateUserController = new AuthenticateUserController;
 const deleteUserController = new DeleteUserController;
 const getUserController = new GetUserController;
+const editUserControoler = new EditUserController;
 
 
 router.get('/ping', (req, res) => { res.json({ pong: true }) });
 
 router.post('/user', AuthHandler, EnsureAsAdmin, createUserControler.handle);
 router.delete('/user', AuthHandler, EnsureAsAdmin, deleteUserController.handle);
-router.get('/user',AuthHandler, EnsureAsAdmin, getUserController.handle);
+router.get('/user', AuthHandler, EnsureAsAdmin, getUserController.handle);
+router.put('/user/edit/:id',AuthHandler, EnsureAsAdmin, editUserControoler.handle);
 router.post('/login', authenticateUserController.handle);
 
