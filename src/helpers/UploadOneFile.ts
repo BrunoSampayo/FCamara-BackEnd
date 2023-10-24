85ccb9c31d13ac8cdc76e680b05d177a3c6ff434
Bruno Sampayo <brunosampayo1040@gmail.com>
2023-10-24 10:08:20 -0300

    feat: method for upload single file jpg

---
+++ src/helpers/UploadOneFile.ts
@@ -0,0 +1,19 @@
+import sharp from "sharp";
+import { unlink } from 'fs/promises'
+
+class UploadOneFile {
+    async execute(file: Express.Multer.File, path: string) {
+        if (file) {
+            const files = file as Express.Multer.File
+            await sharp(files.path).resize(500).toFormat('jpeg').toFile(`${path}${files.filename}.jpg`);
+            console.log(path)
+            await unlink(files.path)
+            return files.filename + ".jpg";
+
+        } else {
+            throw new Error("Upload file error")
+        }
+    }
+}
+
+export { UploadOneFile }
