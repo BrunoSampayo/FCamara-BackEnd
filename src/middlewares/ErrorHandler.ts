97e7e25928226a5924e3fa466449fe0975f32f9d
Bruno Sampayo <brunosampayo1040@gmail.com>
2023-10-23 23:33:32 -0300

    feat: modify error handle to send error rigth

--- src/middlewares/ErrorHandler.ts
+++ src/middlewares/ErrorHandler.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 
 const errorHandler = (err: Error,req: Request,res: Response,next: NextFunction) => {
     
-    res.status(500).json({ error: 'Algo deu errado!' });
+    res.status(500).json({ error: 'Algo deu errado:' + err });
 };
 
 export default errorHandler;
